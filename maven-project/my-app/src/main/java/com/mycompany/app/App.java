ad82e84a8447a8c836466278377e25f835717285
Jayesh Shah <121786340+jayesh271184@users.noreply.github.com>
2023-05-09 00:46:31 +0530

    Update App.java

--- maven-project/my-app/src/main/java/com/mycompany/app/App.java
+++ maven-project/my-app/src/main/java/com/mycompany/app/App.java
@@ -9,5 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World! From Jayesh Shah @ Skyvera Tech : Update 1" );
+        System.out.println( "My Self Jayesh Shah : Update 2" );
     }
 }
